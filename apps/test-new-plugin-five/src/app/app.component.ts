e90a90c0993abd11e958811eb75ce5de6ffde235
Charlie Greenman <charlie@razroo.com>
2023-11-09 20:41:40 -0500

    Add code for angular-16.2.0 create-workspace.
    [Url: razroo.com/community/angular-16.2.0/create-workspace-recipe/create-workspace]

---
+++ apps/test-new-plugin-five/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-new-plugin-five-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'test-new-plugin-five';
+}
